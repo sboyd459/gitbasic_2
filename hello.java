bc5dc61a1866c9df073dd15618e138065b4a02da
Sydney Boyd <sydneybsb21@gmail.com>
2020-04-05 23:55:18 -0400

    reverted back to originals

--- hello.java
+++ hello.java
@@ -2,8 +2,10 @@ public class hello{
 	public void doSomethingInteresting(){
 		//doing interesting stuffs here
 		int a = 10;
-		int b = 20;
+		int b = 20; 
 		a = a * b; 
+		
+		
 	
 	public static void main(String [] args){
 		System.out.println("Hello Git"); 
