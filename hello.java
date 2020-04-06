1d73a8060407915e9ad050ed4ee34fe1d93906e1
sboyd459 <sydneybsb21@gmail.com>
2020-04-05 22:58:54 -0400

    Added some Computation

--- hello.java
+++ hello.java
@@ -1,4 +1,9 @@
 public class hello{
+	public void doSomethingInteresting(){
+		//doing interesting stuffs here
+		int a = 10;
+		int b = 20;
+		a = a + b; 
 	
 	public static void main(String [] args){
 		System.out.println("Hello Git"); 
