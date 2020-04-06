f8aaa182bc4d87e9f600c0940650e31f7312e419
Sydney Boyd <sydneybsb21@gmail.com>
2020-04-06 00:10:12 -0400

    still fixing

--- hello.java
+++ hello.java
@@ -2,16 +2,9 @@ public class hello{
 	public void doSomethingInteresting(){
 		//doing interesting stuffs here
 		int a = 10;
-<<<<<<< HEAD
 		int b = 20; 
 		a = a * b; 
-		
-		
-=======
-		int b = 20;
-		a = a * b; 
->>>>>>> hotfix
-	
+
 	public static void main(String [] args){
 		System.out.println("Hello Git"); 
 	}
